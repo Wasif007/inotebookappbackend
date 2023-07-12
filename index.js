85a64a4c4c820cbbec509665f70e92927b913530
Wasif007 <wasifateeq007@hotmail.com>
2023-07-12 23:23:33 +0500

    basic routes and some schema made for mongoose

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ moongoseConnection();
 
 
 const app = express()
-const port = 27017
+const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Wasif!')
-})
+app.use('/api/auth',require('./routes/auth.js'));
+app.use('/api/notes',require('./routes/notes.js'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
