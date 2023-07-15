6bcc1fc5232c842ade4f84c0b7246852297418e4
Wasif007 <wasifateeq007@hotmail.com>
2023-07-15 22:57:27 +0500

    adding express validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const Users = require("../models/Users");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 //Post request sending
-router.post("/",(req, res) => {
-	console.log(req.body);
-    const user=Users(req.body);
-    user.save();
+router.post("/",[
+  body('email',"Please enter a valid Email").isEmail(),
+  body('password',"Please enter a 5 characters password").isLength({ min: 5 }),
+  body('name',"Username must be more than 3 characters").isLength({ min: 3 }),
+],(req, res) => {
+	const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     res.send(req.body);
 })
 module.exports = router
